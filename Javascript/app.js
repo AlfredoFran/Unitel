3263b7515a033a5a2088ad5a9188ef0c85dafa1a
AlfredoFran <https://github.com/AlfredoFran/Unitel.git>
2024-02-27 12:21:57 -1200

    Unitel

---
+++ Javascript/app.js
@@ -0,0 +1,9 @@
+var n1=1;
+var n2=2;
+var n3=3;
+let mult=n1*n2*n3;
+let restDiv=5%2;
+let media=(n1+n2+n3)/3;
+console.log("Multiplicação de "+n1+","+n2+" e "+n3+" é: "+mult)
+console.log("Resto da divisão de"+" 5 e 2 é: "+ restDiv)
+console.log("Média de " +n1+","+n2+" e "+n3+" é: "+media)
